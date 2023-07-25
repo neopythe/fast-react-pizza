72328cddfd78629a4df0eb29babd5f515d24b6a8
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-07-24 18:29:36 -0600

    Build the AppLayout

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,20 @@ import CreateOrder from "@/features/order/CreateOrder";
 import Menu from "@/features/menu/Menu";
 import Order from "@/features/order/Order";
 
+import AppLayout from "@/ui/AppLayout";
 import Home from "@/ui/Home";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/cart", element: <Cart /> },
-  { path: "/menu", element: <Menu /> },
-  { path: "/order/:orderId", element: <Order /> },
-  { path: "/order/new", element: <CreateOrder /> },
+  {
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/order/:orderId", element: <Order /> },
+      { path: "/order/new", element: <CreateOrder /> },
+    ],
+  },
 ]);
 
 function App() {
