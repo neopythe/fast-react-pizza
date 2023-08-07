5010237bab188640ce4402ed3f389e6b4ac12926
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-08-07 09:30:54 -0600

    Improve type checking for services

--- src/services/apiGeocoding.ts
+++ src/services/apiGeocoding.ts
@@ -1,15 +1,22 @@
+interface Address {
+  locality?: string;
+  city?: string;
+  postcode?: string;
+  countryName?: string;
+}
+
 export async function getAddress({
   latitude,
   longitude,
 }: {
   latitude: number;
   longitude: number;
-}) {
-  const res = await fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
+}): Promise<Address> {
+  const response = await fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`,
   );
-  if (!res.ok) throw Error("Failed getting address");
+  if (!response.ok) throw Error("Failed getting address");
 
-  const data = await res.json();
+  const data = await response.json();
   return data;
 }
--- src/services/apiRestaurant.ts
+++ src/services/apiRestaurant.ts
@@ -1,51 +1,57 @@
+import type { CartItem, Order, Pizza } from "@/types";
+
 const API_URL = "https://react-fast-pizza-api.onrender.com/api";
 
-export async function getMenu() {
-  const res = await fetch(`${API_URL}/menu`);
+export async function getMenu(): Promise<Pizza[]> {
+  const response = await fetch(`${API_URL}/menu`);
 
   // fetch won't throw error on 400 errors (e.g. when URL is wrong), so we need to do it manually. This will then go into the catch block, where the message is set
-  if (!res.ok) throw Error("Failed getting menu");
+  if (!response.ok) throw Error("Failed getting menu");
 
-  const { data } = await res.json();
+  const { data } = await response.json();
   return data;
 }
 
-export async function getOrder(id: string) {
-  const res = await fetch(`${API_URL}/order/${id}`);
-  if (!res.ok) throw Error(`Couldn't find order #${id}`);
+export async function getOrder(id: string): Promise<Order> {
+  const response = await fetch(`${API_URL}/order/${id}`);
+  if (!response.ok) throw Error(`Couldn't find order #${id}`);
 
-  const { data } = await res.json();
+  const { data } = await response.json();
   return data;
 }
 
-export async function createOrder(newOrder: unknown) {
+export async function createOrder(newOrder: {
+  cart: CartItem[];
+  phone: string;
+  priority: boolean;
+}) {
   try {
-    const res = await fetch(`${API_URL}/order`, {
+    const response = await fetch(`${API_URL}/order`, {
       method: "POST",
       body: JSON.stringify(newOrder),
       headers: {
         "Content-Type": "application/json",
       },
     });
-    if (!res.ok) throw Error();
+    if (!response.ok) throw Error();
 
-    const { data } = await res.json();
+    const { data } = await response.json();
     return data;
   } catch {
     throw Error("Failed creating your order");
   }
 }
 
-export async function updateOrder(id: number, updateObj: unknown) {
+export async function updateOrder(id: number, updateObject: unknown) {
   try {
-    const res = await fetch(`${API_URL}/order/${id}`, {
+    const response = await fetch(`${API_URL}/order/${id}`, {
       method: "PATCH",
-      body: JSON.stringify(updateObj),
+      body: JSON.stringify(updateObject),
       headers: {
         "Content-Type": "application/json",
       },
     });
-    if (!res.ok) throw Error();
+    if (!response.ok) throw Error();
 
     // We don't need the data, so we don't return anything
   } catch (err) {
